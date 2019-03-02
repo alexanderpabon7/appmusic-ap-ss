d7069b09fb0c7732604a6275e01c068fdbb47c25
Alexander Pabon <alex312-@hotmail.com>
2019-03-01 21:40:34 -0500

    Commit inicial

---
+++ src/main/java/co/com/poli/appmusicapss/AppmusicApSsApplication.java
@@ -0,0 +1,13 @@
+package co.com.poli.appmusicapss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppmusicApSsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppmusicApSsApplication.class, args);
+	}
+
+}
